578d14fecc7696b78cd318c2cbd4ca5e77858fa0
Shawn Roberts <itsshawnroberts@gmail.com>
2021-06-19 09:34:13 -0400

    Assignment 5.3 Finished

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,15 +1,24 @@
+# Shawn Roberts
+# 06/19/2021
+# Module 5
+# Pytech Insert
+# my GitHub: git@github.com:itsshawnroberts/csd-310.git
+
+
+# This function allows us to coneect to my MongoDB server cluster.
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.ggu9b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
 client = MongoClient(url)
 db = client.pytech
 
+# My studentData variables store the information to want to push to my MongoDB cluster.
 studentData = {
     "student_id": "1007",
     "first_name": "Tony",
     "last_name": "Stark"
      }
-
+# This function allows Python to push my studentData information to my MongoDB cluster called students.
 result = db.students.insert_one(studentData)
 print("-- INSERT STATEMENTS --")
 print(f"Inserted student record {studentData['first_name']} {studentData['last_name']} into students collection with document_id {result.inserted_id}")
--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,22 +1,34 @@
 
+# Shawn Roberts
+# 06/19/2021
+# Module 5
+# 5.3 Pytech Queries
+# my GitHub: git@github.com:itsshawnroberts/csd-310.git
+
+
+# Establishing connection to MongoDB to retrieve data.
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.ggu9b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
 client = MongoClient(url)
 db = client.pytech
 
-docs = db.students.find({})
-print ("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
-for doc in docs:
- print(f"Student ID: {doc['student_id']}")
- print(f"First Name: {doc['first_name']}")
- print(f"Last Name:  {doc['last_name']}\n")
+# Telling Python to find the documents we want to we in the output.
+docs = db.students.find()
 
-print ("-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY -- ")
+
+for doc in docs:
+    print(f"Student ID: {doc['student_id']}")
+    print(f"First Name: {doc['first_name']}")
+    print(f"Last Name: {doc['last_name']}\n")
+
+# This function allows us to find a certain piece of data within our student document.
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find_one() QUERY -- ")
 doc = db.students.find_one({"student_id": "1007"})
+
 print(f"Student ID: {doc['student_id']}")
 print(f"First Name: {doc['first_name']}")
-print(f"Last Name:  {doc['last_name']}\n")
-
+print(f"Last Name: {doc['last_name']}\n\n")
 
 print("End of Program, press any key to continue...")
